d05c4e8e9783a49b1ea79813362d9d48962d6704
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-04-30 21:42:29 +0100

    Update: Filter orders by customer and order_status

--- pizza_mart/settings.py
+++ pizza_mart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
     'customers',
     'pizza_orders',
 ]
--- pizza_orders/views.py
+++ pizza_orders/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from pizza_orders.models import Order
 from pizza_orders.serializers import OrderSerializer
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class OrderReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
@@ -10,3 +12,5 @@ class OrderReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['order_status', 'customer']
