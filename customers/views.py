58ebfb1fc9382d05fd0be25f7e712c22af403cb3
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-04-30 14:31:45 +0100

    Update: Customer ViewSet, Routers, Urls & Serializers

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from customers.models import Customer
+from customers.serializers import CustomerSerializer
 
 # Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing customer instances.
+    """
+    serializer_class = CustomerSerializer
+    queryset = Customer.objects.all()
+
--- pizza_mart/urls.py
+++ pizza_mart/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from customers.views import CustomerViewSet
+
+customer_router = DefaultRouter()
+customer_router.register(r'customers',  CustomerViewSet, basename='customer')
 
 urlpatterns = [
+    # path('customers/', include('customers.urls', namespace='customers'))
     path('admin/', admin.site.urls),
+    path('api/', include(customer_router.urls)),
+
 ]
