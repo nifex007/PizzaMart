de7f44c3cc9cfedee87ef29f14b15078316a2aba
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-04-30 02:05:38 +0100

    Update: Created customers app, model

---
+++ customers/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Customer(AbstractUser):
+    full_name = models.CharField(max_length=255)
+    address = models.TextField(blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    phone = models.CharField(max_length=14, blank=False, null=False, unique=True)
+
+
+    def __str__(self):
+        return '{} - {}'.format(self.full_name, self.phone)
