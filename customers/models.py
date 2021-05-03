a5488bb2d6a59698a7e68fea1829f4d748abb04a
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-05-03 13:38:05 +0100

    Bug: Fixed DETAIL:  Key (username)=() already exists.

--- customers/models.py
+++ customers/models.py
@@ -6,8 +6,13 @@ from django.contrib.auth.models import AbstractUser
 class Customer(AbstractUser):
     full_name = models.CharField(max_length=255)
     address = models.TextField(blank=False, null=False)
-    email = models.EmailField(max_length=254, blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False, unique=True)
     phone = models.CharField(max_length=14, blank=False, null=False, unique=True)
+
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
 
 
     def __str__(self):
