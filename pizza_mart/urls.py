b758aaf999e77a4d3b89d6751a8288767831447b
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-05-04 02:10:29 +0100

    Renamed order_read url to order-list

--- pizza_mart/urls.py
+++ pizza_mart/urls.py
@@ -24,7 +24,7 @@ from pizza_orders.views import OrderReadOnlyViewSet, OrderViewSet
 main_router = routers.DefaultRouter()
 
 main_router.register(r'customers/?', CustomerViewSet, basename='customer')
-main_router.register(r'orders_read/?', OrderReadOnlyViewSet, basename='read_order')
+main_router.register(r'orders-list/?', OrderReadOnlyViewSet, basename='orders_list')
 main_router.register(r'orders/?', OrderViewSet, basename='order')
 
 urlpatterns = [
